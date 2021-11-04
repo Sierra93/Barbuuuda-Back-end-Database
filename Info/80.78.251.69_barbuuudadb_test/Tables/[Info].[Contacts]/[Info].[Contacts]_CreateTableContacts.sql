dde914184cd75391b8b3beda913eac05a6f70555
Verbitski A <eternity.2019@mail.ru>
2021-11-04 08:39:12 +0300

    Изменён тип поля Info.Contacts.INN c varchar на bigint.

--- Info/80.78.251.69_barbuuudadb_test/Tables/[Info].[Contacts]/[Info].[Contacts]_CreateTableContacts.sql
+++ Info/80.78.251.69_barbuuudadb_test/Tables/[Info].[Contacts]/[Info].[Contacts]_CreateTableContacts.sql
@@ -4,5 +4,5 @@ CREATE TABLE if not exists "Info"."Contacts" (
     "Email" varchar(100) not null default 'info.barbuuuda@gmail.com',
     "Work" varchar(200) not null default 'Работаем по будням с 10:00 до 20:00 по Мск',
     "Creator" varchar(100) not null default 'Тимошенко Антон Иванович',
-    "INN" varchar(50) not null default '771889326195.'
+    "INN" bigint not null default 771889326195
 );
