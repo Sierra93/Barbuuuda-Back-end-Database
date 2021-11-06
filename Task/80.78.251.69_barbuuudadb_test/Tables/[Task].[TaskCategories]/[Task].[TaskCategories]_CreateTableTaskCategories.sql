bfa4dd4b522f8319d9a1d9c15db37d51199894c9
Verbitski A <eternity.2019@mail.ru>
2021-11-06 14:07:54 +0300

    Исправлены синтаксические ошибки.

--- Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskCategories]/[Task].[TaskCategories]_CreateTableTaskCategories.sql
+++ Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskCategories]/[Task].[TaskCategories]_CreateTableTaskCategories.sql
@@ -3,5 +3,5 @@ create table if not exists "Task"."TaskCategories" (
 	category_name varchar(100) NOT NULL DEFAULT ''::character varying,
 	category_code varchar(100) NOT NULL DEFAULT ''::character varying,
 	specializations jsonb NULL,
-	url text NULL DEFAULT ''::text,   
+	url text NULL DEFAULT ''::text   
 );
--- Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskTypes]/[Task].[TaskTypes]_CreateTableTaskTypes.sql
+++ Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskTypes]/[Task].[TaskTypes]_CreateTableTaskTypes.sql
@@ -1,5 +1,5 @@
 create table if not exists "Task"."TaskTypes" (
-    type_id serial4 primaty key unique NOT NULL,
+    type_id serial4 primary key unique NOT NULL,
 	type_name varchar(100) NOT NULL DEFAULT ''::character varying,
-	type_code varchar(100) NOT NULL DEFAULT ''::character varying,    
+	type_code varchar(100) NOT NULL DEFAULT ''::character varying   
 );
