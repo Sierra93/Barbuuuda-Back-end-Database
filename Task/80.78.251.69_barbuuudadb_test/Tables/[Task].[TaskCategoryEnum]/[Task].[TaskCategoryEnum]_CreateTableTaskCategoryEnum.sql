e59a6c87d546712dea6322844cdb7e56464a1b4a
Verbitski A <eternity.2019@mail.ru>
2021-11-06 17:44:56 +0300

    Исправлены ошибки в названия таблиц Task.TaskCategoryEnum, Task.TaskStatusEnum, Task.TaskTypeEnum

--- Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskCategoryEnum]/[Task].[TaskCategoryEnum]_CreateTableTaskCategoryEnum.sql
+++ Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskCategoryEnum]/[Task].[TaskCategoryEnum]_CreateTableTaskCategoryEnum.sql
@@ -1,4 +1,4 @@
-create table if not exists "Task"."TaskCategoriesEnum" (
+create table if not exists "Task"."TaskCategoryEnum" (
     "Id" serial primary key unique,
     "Name" varchar(128) not null,
     "Description" varchar(128)
--- Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskStatusEnum]/[Task].[TaskStatusEnum]_CreateTableTaskStatusEnum.sql
+++ Task/80.78.251.69_barbuuudadb_test/Tables/[Task].[TaskStatusEnum]/[Task].[TaskStatusEnum]_CreateTableTaskStatusEnum.sql
@@ -1,4 +1,4 @@
-create table if not exists "Task"."TaskTypesEnum" (    
+create table "Task"."TaskStatusEnum" (    
     "Id" serial primary key unique,
     "Name" varchar(128) not null,
     "Description" varchar(128)  
