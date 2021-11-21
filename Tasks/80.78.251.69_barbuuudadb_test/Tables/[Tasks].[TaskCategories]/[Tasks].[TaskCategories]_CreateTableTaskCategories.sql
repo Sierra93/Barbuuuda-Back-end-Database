079d16adb3a450a36391852724979e7f70775864
Verbitski A <eternity.2019@mail.ru>
2021-11-22 00:02:55 +0300

    Добавлены и проведены миграции для создания таблицы Info.ExecutorStatistic. Исправлена ошибка, когда поле Tasks.TaskCategories.category_code не являлось уникальным.

--- Tasks/80.78.251.69_barbuuudadb_test/Tables/[Tasks].[TaskCategories]/[Tasks].[TaskCategories]_CreateTableTaskCategories.sql
+++ Tasks/80.78.251.69_barbuuudadb_test/Tables/[Tasks].[TaskCategories]/[Tasks].[TaskCategories]_CreateTableTaskCategories.sql
@@ -1,7 +1,9 @@
-create table if not exists "Tasks"."TaskCategories" (
-    category_id serial4 primary key unique NOT NULL,
+CREATE TABLE IF NOT EXISTS "Tasks"."TaskCategories" (
+	category_id serial4 NOT NULL,
 	category_name varchar(100) NOT NULL DEFAULT ''::character varying,
 	category_code varchar(100) NOT NULL DEFAULT ''::character varying,
 	specializations jsonb NULL,
-	url text NULL DEFAULT ''::text   
+	url text NULL DEFAULT ''::text,
+	CONSTRAINT "TaskCategories_category_code_key" UNIQUE (category_code),	
+	CONSTRAINT "TaskCategories_pkey" PRIMARY KEY (category_id)
 );
